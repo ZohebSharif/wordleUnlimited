b48c3f84dc5f5b5bfa352fdd1725e02625650063
Zoheb Sharif <sharifzoheb@gmail.com>
2025-02-10 22:55:40 -0800

    Added comments to act as a guide

---
+++ wordleUnlimited.java
@@ -0,0 +1,19 @@
+public class wordleUnlimited{
+    public static void main (String args[])
+    {
+        // Will start off as a console application
+
+        // A word is selected at random
+
+        // A gameplay loop will start.
+
+        // We get a word from the txt file at random.
+
+        // * Guesses will be taken 5 letters at a time and those guesses have to be found within the file, otherwise it is an invalid word
+
+        // In console, correct guesses will be denoted as 'C'. Existing characters will be 'E', and incorrect characters will be 'X'/
+
+        // At the end of the game, we will prompt user if they would like to continue.
+        
+    }
+}
